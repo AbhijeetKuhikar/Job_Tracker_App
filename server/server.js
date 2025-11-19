d4bb51e2f5e3a5d3b77cd08f91c6ea803cb84263
AbhijeetKuhikar <abhijeetkuhikar015@gmail.com>
2025-11-19 09:47:37 +0530

    user Actions Updated

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import cors from "cors"
 import "./database/conn.js"
 
 import { userRouter } from "./routers/userRouter.js"
-import { companyRouter } from "./routers/companyRouter.js"
+import { jobRouter } from "./routers/jobRouter.js"
+// import { companyRouter } from "./routers/companyRouter.js"
 // import { adminRouter } from "./routers/adminRouter.js"
 
 dotenv.config({ path: "./config.env" })
@@ -14,6 +15,8 @@ const app = express()
 let port = process.env.PORT || 5012
 
 app.use(express.static("public"))
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -28,16 +31,20 @@ app.use(cors(corsOptions))
 
 app.use("/user", userRouter)
 
-app.use("/company", companyRouter)
+app.use("/job", jobRouter)
+
+app.use(express.static("uploads"))
+
+// app.use("/company", companyRouter)
 
 // app.use("/admin", adminRouter)
 
 // handle 404 route
 app.use((req, res) => {
-    console.log("user trying to access invalid route !")
-    res.status(404).json({ message: "content/route not found !" })
+  console.log("user trying to access invalid route !")
+  res.status(404).json({ message: "content/route not found !" })
 })
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port} !`)
+  console.log(`server is running on port ${port} !`)
 })
