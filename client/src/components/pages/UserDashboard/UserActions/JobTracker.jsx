89d426d58d0bf11485275159734ba2881f1846b9
AbhijeetKuhikar <abhijeetkuhikar015@gmail.com>
2025-11-23 20:17:03 +0530

    Profile

--- client/src/components/pages/UserDashboard/UserActions/JobTracker.jsx
+++ client/src/components/pages/UserDashboard/UserActions/JobTracker.jsx
@@ -1,27 +1,11 @@
-import express from "express"
-import { test, handleUserRegister, handleOTPVerification, handleUserLogin, handleResetPasswordRequest, handleOTPForPasswordReset, handleUserFileUpload, fetchProfile } from "../controllers/userController.js"
-import { AuthUser } from "../middlewares/AuthUser.js"
-import { upload } from "../config/multerConfig.js"
+import React from 'react'
 
-let userRouter = express.Router()
+const JobTracker = () => {
+  return (
+    <div>
+      <h1>this is job tracker</h1>
+    </div>
+  )
+}
 
-userRouter.get("/test", test)
-
-userRouter.post("/register", handleUserRegister)
-
-userRouter.post("/verify-otp", handleOTPVerification)
-
-userRouter.post("/user-login", handleUserLogin)
-
-userRouter.post("/password-reset-request", handleResetPasswordRequest)
-
-userRouter.post("/verify-reset-password-request", handleOTPForPasswordReset)
-
-// to upload resume/profie/docs we need to verfiy the user
-
-userRouter.post("/upload-file/:file_type", AuthUser, upload.single("file"), handleUserFileUpload)
-// only profile_picture and resume
-
-userRouter.get("/fetch-urser-profile", AuthUser, fetchProfile)
-
-export { userRouter }
+export default JobTracker
